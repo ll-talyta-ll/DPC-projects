72d0c20d7cc4717be3fb1ffad3787c75541155c8
Talyta Scaramuzzo <64484600+ll-talyta-ll@users.noreply.github.com>
2022-05-18 20:46:34 -0300

    Add files via upload

---
+++ my.controls/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System.Windows;
+
+namespace my.controls
+{
+    /// <summary>
+    /// Interação lógica para MainWindow.xam
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void OnExitClick(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown(0);
+        }
+
+        private void OnAboutClick(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Como podemos te ajudar?");
+        }
+        private void OnCadastroClick(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Vamos Começar o cadastro!");
+        }
+        private void OnInfoDPCClick(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Olá, somos a DPC Consultoria, como podemos ajudar?");
+        }
+         private void OnEmpresasClick(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("olá, trabalhamos com grandes clientes");
+        }
+        private void OnServicosBIMClick(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Olá, somos a DPC Consultoria, fazemos projetos para grandes empresas utilizando os Serviços BIM!");
+        }
+    }
+}
